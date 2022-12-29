a69fb245de3bfaa7a15180543737269cb3637631
faelvinicius <rafaelviniciuscardoso@gmail.com>
2022-12-29 14:05:47 -0300

    update variable name in provider

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
-variable "login_username" {}
-variable "login_password" {}
+variable "vault_login" {}
+variable "vault_password" {}
 variable "vault_addr" {}
 
 provider "vault" {
   address = var.vault_addr
   auth_login {
-    path = "auth/userpass/login/${var.login_username}"
+    path = "auth/userpass/login/${var.vault_login}"
 
     parameters = {
-      password = var.login_password
+      password = var.vault_password
     }
   }
 }
